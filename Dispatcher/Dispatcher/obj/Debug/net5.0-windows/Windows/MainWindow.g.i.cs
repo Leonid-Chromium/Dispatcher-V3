6529f4012ad45963f7d521163823daf3f3315292
Leonid-Chromium <l.khromenko@gmail.com>
2022-02-07 10:31:24 +0700

    compilling

--- Dispatcher/Dispatcher/obj/Debug/net5.0-windows/Windows/MainWindow.g.i.cs
+++ Dispatcher/Dispatcher/obj/Debug/net5.0-windows/Windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace Dispatcher {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Dispatcher;V1.0.0.0;component/windows/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Dispatcher;component/windows/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Windows\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
