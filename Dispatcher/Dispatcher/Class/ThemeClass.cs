1fcd3a9017b7bbd462ff50eb79db9aecf0e725b1
Leonid-Chromium <l.khromenko@gmail.com>
2022-02-08 16:56:07 +0700

    AddThemeClass

---
+++ Dispatcher/Dispatcher/Class/ThemeClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Dispatcher.Class
+{
+    class ThemeClass
+    {
+        public static void MyThemeChange(string newTheme)
+        {
+            // определяем путь к файлу ресурсов
+            var uri = new Uri("Theme\\" + newTheme + ".xaml", UriKind.Relative);
+            // загружаем словарь ресурсов
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            // очищаем коллекцию ресурсов приложения
+            Application.Current.Resources.Clear();
+            // добавляем загруженный словарь ресурсов
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+            // сохраняем настройку
+
+            //Properties.Settings.Default.ThemeSettings = newTheme;
+            //Properties.Settings.Default.Save();
+            //Properties.Settings.Default.Reload();
+
+
+        }
+    }
+}
