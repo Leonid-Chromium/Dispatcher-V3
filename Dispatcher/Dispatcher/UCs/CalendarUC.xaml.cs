17ef963801e789e5dfa5aef012a61ff5c5347c03
CreatorDL <dmitriiliukshenko@gmail.com>
2022-03-10 16:41:45 +0700

    Add Calendar

---
+++ Dispatcher/Dispatcher/UCs/CalendarUC.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dispatcher.UCs
+{
+    /// <summary>
+    /// Логика взаимодействия для CalendarUC.xaml
+    /// </summary>
+    public partial class CalendarUC : UserControl
+    {
+        public CalendarUC()
+        {
+            InitializeComponent();
+        }
+
+        private void FirstDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
