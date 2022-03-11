82f8e3aa3c68de4b45cc53aef50b01a4f23b0ccf
Leonid-Chromium <l.khromenko@gmail.com>
2022-03-11 09:00:54 +0700

    VerifyMergeBySuperUC

--- Dispatcher/Dispatcher/obj/Debug/net5.0-windows/Windows/AuthorizationWindow.g.i.cs
+++ Dispatcher/Dispatcher/obj/Debug/net5.0-windows/Windows/AuthorizationWindow.g.i.cs
@@ -87,13 +87,13 @@ namespace Dispatcher.Windows {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.10.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Dispatcher;component/windows/authorizationwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Dispatcher;V1.0.0.0;component/windows/authorizationwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Windows\AuthorizationWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -103,7 +103,7 @@ namespace Dispatcher.Windows {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.10.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
--- Dispatcher/Dispatcher/obj/Debug/net5.0-windows/Windows/WorkWindow.g.i.cs
+++ Dispatcher/Dispatcher/obj/Debug/net5.0-windows/Windows/WorkWindow.g.i.cs
@@ -47,13 +47,13 @@ namespace Dispatcher.Windows {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.10.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Dispatcher;component/windows/workwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Dispatcher;V1.0.0.0;component/windows/workwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Windows\WorkWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -63,7 +63,7 @@ namespace Dispatcher.Windows {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.10.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
