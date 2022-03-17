2932aeb044812db63348e455cf75c4df17d473f0
Dmitry Liukshenko <dmitriiliukshenko@gmail.com>
2022-03-17 13:06:03 +0700

    Save Button

--- Dispatcher/Dispatcher/UCs/BathesUC.xaml.cs
+++ Dispatcher/Dispatcher/UCs/BathesUC.xaml.cs
@@ -51,19 +51,9 @@ namespace Dispatcher.UCs
 
                     TBLastCount.Text = dataTable.Rows[0].ItemArray[0].ToString().Trim();
 
-                    dataTable = SQL.ReturnDT(@"SELECT Equipments.IdEquipment,Equipments.InventoryNumber,Equipments.Name From Equipments
-                        RIGHT JOIN (SELECT OperationHistory.IdBatch ,OperationHistory.LastCount ,OperationHistory.IdEquipment
-                        FROM OperationHistory Right join (SELECT OperationHistory.IdBatch,MAX(OperationHistory.IdRecording) AS 'LastIdRecording'
-		                FROM OperationHistory Group by IdBatch) AS LastRecording ON LastRecording.LastIdRecording = OperationHistory.IdRecording)
-                        AS LastOperationInfo on Equipments.IdEquipment = LastOperationInfo.IdEquipment Where LastOperationInfo.IdBatch = " + TBIDBathes.Text, App.configuration.SQLConnectionString, out ex);
-
-                    TBIDEquipment.Text = dataTable.Rows[0].ItemArray[0].ToString().Trim();
-                    TBNumberEquipment.Text = dataTable.Rows[0].ItemArray[1].ToString().Trim();
-                    TBNameEquipment.Text = dataTable.Rows[0].ItemArray[2].ToString().Trim();
-
                     dataTable = SQL.ReturnDT(@"SELECT maxNum = CASE WHEN MAX(Operations.Number) = null THEN 0 ELSE MAX(Operations.Number) end,
                         OperationHistory.IdBatch
-                        FROM OperationHistory Left JOIN Operations on Operations.IdOperation = OperationHistory.IdOperation 
+                        FROM OperationHistory Left JOIN Operations on Operations.Number = OperationHistory.IdOperation 
                         WHERE OperationHistory.IdBatch = " + TBIDBathes.Text + " Group by IdBatch", App.configuration.SQLConnectionString, out ex);
                     
                     TBNumberOperation.Text = dataTable.Rows[0].ItemArray[0].ToString().Trim();
@@ -75,12 +65,60 @@ namespace Dispatcher.UCs
                         WHERE OperationHistory.IdBatch = " + TBIDBathes.Text + " Group by IdBatch ) AS LastNumberOperation on (LastNumberOperation.maxNum = Operations.Number)", App.configuration.SQLConnectionString, out ex);
 
                     TBNameOperation.Text = dataTable.Rows[0].ItemArray[0].ToString().Trim();
+
+                    try
+                    {
+                        dataTable = SQL.ReturnDT(@"SELECT 
+TechnologicalMaps.NameTM, 
+Routing.Mode, 
+Routing.Note,
+Equipments.IdEquipment,
+Equipments.InventoryNumber,
+Equipments.Name
+FROM Batchs
+JOIN OperationHistory ON OperationHistory.IdBatch = Batchs.IdBatch
+JOIN Operations ON Operations.IdOperation = OperationHistory.IdOperation
+JOIN Routing ON Routing.IdRouting = Operations.IdRouting
+JOIN TechnologicalMaps ON TechnologicalMaps.IdTM = Routing.IdRouting
+JOIN Equipments ON Equipments.IdEquipment = OperationHistory.IdEquipment
+WHERE Batchs.IdBatch = " + TBIDBathes.Text + " AND Operations.IdOperation = " + TBNumberOperation.Text, App.configuration.SQLConnectionString, out ex);
+                        TBTechnologiesMaps.Text = dataTable.Rows[0].ItemArray[0].ToString().Trim();
+                        TBMode.Text = dataTable.Rows[0].ItemArray[1].ToString().Trim();
+                        TBNoteRouting.Text = dataTable.Rows[0].ItemArray[2].ToString().Trim();
+                        TBIDEquipment.Text = dataTable.Rows[0].ItemArray[3].ToString().Trim();
+                        TBNumberEquipment.Text = dataTable.Rows[0].ItemArray[4].ToString().Trim();
+                        TBNameEquipment.Text = dataTable.Rows[0].ItemArray[5].ToString().Trim();
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Партия закончена или не существует");                        
+                    }
+                    
+
+
+                    
                 }
             }
             catch (Exception ex)
             {
 
                 MessageBox.Show(Convert.ToString(ex));
+            }
+            
+        }
+
+        private void btn_Save_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                 
+                SQL.NoReturn(@"INSERT INTO OperationHistory(IdOperation, IdBatch, IdEmployee, IdEquipment,LastCount) VALUES(" + TBNumberOperation.Text + "," + TBIDBathes.Text + "," + TBIdEmployes.Text + "," + TBIDEquipment.Text + "," + TBLastCount.Text + ")", App.configuration.SQLConnectionString, out string ex);
+                MessageBox.Show("ВЫ сохранили запись");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(Convert.ToString(ex));        
             }
             
         }
