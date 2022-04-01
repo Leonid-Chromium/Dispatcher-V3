8fbd4c3e114d60baadb5117873c78065b8164c57
CreatorDL <dmitriiliukshenko@gmail.com>
2022-04-01 16:00:37 +0700

    Create Design SimpleScannerUC (Add gitignore)

---
+++ Dispatcher/Dispatcher/UCs/SimpleScannerUC.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dispatcher.UCs
+{
+    /// <summary>
+    /// Логика взаимодействия для SimpleScannerUC.xaml
+    /// </summary>
+    public partial class SimpleScannerUC : UserControl
+    {
+        public SimpleScannerUC()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConfirm_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- Dispatcher/Dispatcher/obj/Debug/net5.0-windows/UCs/CalendarUC.g.i.cs
+++ Dispatcher/Dispatcher/obj/Debug/net5.0-windows/UCs/CalendarUC.g.i.cs
@@ -79,13 +79,13 @@ namespace Dispatcher.UCs {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Dispatcher;component/ucs/calendaruc.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Dispatcher;V1.0.0.0;component/ucs/calendaruc.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\UCs\CalendarUC.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -95,7 +95,7 @@ namespace Dispatcher.UCs {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
